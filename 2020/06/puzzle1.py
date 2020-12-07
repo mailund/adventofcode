cb1843ca9924d6f6a1f7e84d9d5ba654204f6a25
Thomas Mailund <mailund@birc.au.dk>
2020-12-07 13:03:10 +0100

    notting

--- 2020/06/puzzle1.py
+++ 2020/06/puzzle1.py
@@ -5,7 +5,7 @@ import string
 def solve_puzzle(infile, set_update, set_init):
     return sum(len(ans) for ans in
         (reduce(
-           lambda answers, ans: set_update(answers, set(ans)), 
+           lambda answers, ans: set_update(answers, set(ans)),
            group.split(),
            set_init
          ) for group in infile.read().split('\n\n')
@@ -18,4 +18,3 @@ def puzzle2(infile):
 
 print('Puzzle #1:', puzzle1(open('test.txt')))
 print('Puzzle #2:', puzzle2(open('test.txt')))
-
